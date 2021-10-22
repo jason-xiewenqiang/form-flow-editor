628ccb5c4ac6c840f67d05f7dcd688b486875569
xiewenqiang <xiewenqiang@xbrother.com>
2021-10-22 17:55:57 +0800

    gantt

--- src/views/gantt/Index.vue
+++ src/views/gantt/Index.vue
@@ -1,6 +1,18 @@
 <template>
   <div class="gantt">
     <div class="gantt-no" ref="ganttRef"></div>
+    <el-dialog title="提示" v-model="dialogVisible" width="30%" :before-close="handleClose">
+      <span>这是一段信息</span>
+      <template #footer>
+        <span class="dialog-footer">
+          <el-button @click="dialogVisible = false">取 消</el-button>
+          <el-button type="primary" @click="dialogVisible = false">确 定</el-button>
+        </span>
+      </template>
+    </el-dialog>
+    <transition name="el-fade-in-linear">
+      <Menu :x="menuX" :y="menuY" v-show="menuVisible" @menu-item="handleItemClick" />
+    </transition>
   </div>
 </template>
 
@@ -9,31 +21,63 @@ import { defineComponent, onMounted, ref } from 'vue';
 import { gantt } from 'dhtmlx-gantt';
 import 'dhtmlx-gantt/codebase/dhtmlxgantt.css';
 import Dayjs from 'dayjs';
+import Menu from './Menu.vue';
 
 export default defineComponent({
+  components: {
+    Menu
+  },
   setup() {
     const ganttRef = ref<HTMLElement | null>(null);
+    const dialogVisible = ref<boolean>(false);
+    const menuVisible = ref<boolean>(false);
+    const menuX = ref<number>(0);
+    const menuY = ref<number>(0);
+    const handleItemClick = (item: any) => {
+      menuVisible.value = false;
+      dialogVisible.value = true;
+    };
+    const handleClose = (done: any) => {
+      done();
+    };
     const tasks = {
       data: [
-        { id: 1, text: '任务 1', start_date: '17-10-2021', duration: 3, progress: 0.6 },
-        { id: 2, text: '任务 2', start_date: '20-10-2021', duration: 10, progress: 0.4 }
-      ],
-      links: []
+        { id: 1, text: '任务 1', start_date: '2021-10-17', duration: 3, progress: 0.6 },
+        { id: 2, text: '任务 2', start_date: '2021-10-20', duration: 10, progress: 0.4 }
+      ]
     };
     onMounted(() => {
       if (ganttRef.value) {
-        // gantt.config.xml_date = '%Y-%m-%d';
-        // gantt.config.date_scale = '%Y-%m';
+        gantt.config.xml_date = '%Y-%m-%d';
         gantt.config.show_links = false;
         gantt.config.drag_progress = false;
         gantt.config.scale_height = 90;
+        gantt.config.duration_unit = 'day';
+        gantt.config.duration_step = 1;
         var weekScaleTemplate = function (date: any) {
+          const mouthMap: { [key: string]: string } = {
+            Jan: '一月',
+            Feb: '二月',
+            Mar: '三月',
+            Apr: '四月',
+            May: '五月',
+            Jun: '六月',
+            Jul: '七月',
+            Aug: '八月',
+            Sept: '九月',
+            Oct: '十月',
+            Nov: '十一月',
+            Dec: '十二月'
+          };
           // 可以时使用dayjs 处理返回
-          console.log('date', date);
-          var dateToStr = gantt.date.date_to_str('%d %M');
+          var dateToStr = gantt.date.date_to_str('%d');
+          var mToStr = gantt.date.date_to_str('%M');
           var endDate = gantt.date.add(gantt.date.add(date, 1, 'week'), -1, 'day');
+          console.log(mToStr(date));
           // 处理一下月份
-          return dateToStr(date) + ' - ' + dateToStr(endDate);
+          return `${dateToStr(date)} 号 - ${dateToStr(endDate)} 号 (${
+            mouthMap[mToStr(date) as string]
+          })`;
         };
         var dayFormat = function (date: any) {
           const weeks = ['星期日', '星期一', '星期二', '星期三', '星期四', '星期五', '星期六'];
@@ -58,22 +102,51 @@ export default defineComponent({
           {
             name: 'start_date',
             label: '时间',
-            width: 200,
+            width: '*',
             align: 'center',
             template: function (obj: any) {
               return obj.start_date;
             }
           },
           {
-            name: 'add',
-            label: '',
-            width: 60,
+            name: 'progress',
+            label: '进度',
+            width: '*',
             align: 'center',
             template: function (obj: any) {
-              return obj.start_date;
+              return `${obj.progress * 100}%`;
             }
           }
         ];
+        gantt.attachEvent(
+          'onEmptyClick',
+          function (e) {
+            //any custom logic here
+            menuVisible.value = false;
+          },
+          {}
+        );
+        gantt.attachEvent(
+          'onContextMenu',
+          function (taskId, linkId, event) {
+            var x = event.clientX + document.body.scrollLeft + document.documentElement.scrollLeft,
+              y = event.clientY + document.body.scrollTop + document.documentElement.scrollTop;
+            // 判断要是在树上的右键菜单才有效果
+            if (taskId && event.target.className === 'gantt_tree_content') {
+              console.log('task ContentMenu', taskId, linkId, event);
+              menuX.value = x;
+              menuY.value = y;
+              menuVisible.value = true;
+            }
+
+            if (taskId || linkId) {
+              return false;
+            }
+
+            return true;
+          },
+          {}
+        );
         gantt.attachEvent(
           'onBeforeLightbox',
           function (id) {
@@ -89,25 +162,49 @@ export default defineComponent({
           },
           {}
         );
-        gantt.config.duration_unit = 'day';
-        gantt.config.duration_step = 1;
+        gantt.attachEvent(
+          'onTaskDblClick',
+          function (id, e) {
+            console.log('id', id, e);
+            dialogVisible.value = true;
+            return false;
+          },
+          {}
+        );
         gantt.plugins({
-          tooltip: true,
           marker: true
         });
         gantt.addMarker({
           start_date: new Date(),
           text: '今日'
         });
-        gantt.templates.tooltip_text = function (start, end, task) {
-          return '<b>Task:</b> ' + task.text + '<br/><b>Duration:</b> ' + task.duration;
-        };
         gantt.init(ganttRef.value);
         gantt.parse(tasks);
+        gantt.addTask(
+          {
+            id: 10,
+            text: 'Task #5',
+            start_date: '2021-10-19',
+            duration: 4,
+            progress: 0.6
+          },
+          1,
+          1
+        );
+        gantt.eachTask((task) => {
+          task.$open = true;
+        });
+        gantt.render();
       }
     });
     return {
-      ganttRef
+      ganttRef,
+      dialogVisible,
+      handleClose,
+      menuY,
+      menuX,
+      menuVisible,
+      handleItemClick
     };
   }
 });
---
+++ src/views/gantt/Menu.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="menu" :style="{ left: x + 'px', top: y + 'px' }">
+    <el-menu
+      @select="handleSelect"
+      background-color="#545c64"
+      text-color="#fff"
+      active-text-color="#fff"
+    >
+      <el-menu-item index="2-1">新增任务</el-menu-item>
+      <el-menu-item index="2-2">编辑任务</el-menu-item>
+      <el-menu-item index="2-3">删除任务</el-menu-item>
+    </el-menu>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+export default defineComponent({
+  props: {
+    x: {
+      type: Number,
+      default: 0
+    },
+    y: {
+      type: Number,
+      default: 0
+    }
+  },
+  emits: ['menu-item'],
+  setup(props, ctx) {
+    const handleSelect = (action: string) => {
+      ctx.emit('menu-item', action);
+    };
+    return {
+      handleSelect
+    };
+  }
+});
+</script>
+
+<style lang="less" scoped>
+.menu {
+  position: fixed;
+  transition: all 1s ease;
+  ::v-deep(.el-menu-item) {
+    height: 40px;
+    line-height: 40px;
+    width: 200px;
+  }
+}
+</style>
